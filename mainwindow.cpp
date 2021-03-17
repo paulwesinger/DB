4c342dcbeecd6700844f6619c6bd3191d7a81990
Paul Wiesinger <paul@localhost.localdomain>
2021-03-17 21:54:41 +0100

    Acivate Signal Slots

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QStringList>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    init();
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +15,19 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::openDialog() {
+
+    qDebug("Operation timed out or an error occurred for port");
+
+    QStringList fileNames;
+
+    fDialog = new  QFileDialog(this,"Open a file...");
+    if (fDialog->exec() )
+        fileNames = fDialog->selectedFiles();
+}
+
+// private Methods
+void MainWindow::init() {
+
+    //connect(ui->loadButton,&QPushButton::click,this,&MainWindow::openialog);
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFileDialog>
+#include <QPushButton>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +17,12 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+public slots:
+    void openDialog();
+
 private:
+    void init();
     Ui::MainWindow *ui;
+    QFileDialog * fDialog;
 };
 #endif // MAINWINDOW_H
